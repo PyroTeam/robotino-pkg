d72502aa4a744183df1e123b411506731e0e2207
robotino <polytechrobocupteam@gmail.com>
2017-10-16 09:34:48 +0200

    PowerReadings

---
+++ robotino_node/src/GyroExtROS.cpp
@@ -0,0 +1,39 @@
+/**
+ * \file 		GyroExtROS.cpp
+ *
+ * \brief
+ *
+ * \author		Coelen Vincent (vincent.coelen@polytech-lille.net)
+ * \date		2017-05-31
+ * \copyright   2017, Association de Robotique de Polytech Lille All rights reserved
+ * \version
+ */
+
+#include "GyroExtROS.h"
+
+GyroExtROS::GyroExtROS():rec::robotino::api2::GyroscopeExt()
+{
+    gyro_pub_ = nh_.advertise<robotino_msgs::GyroReadings>("gyro", 1, false);
+}
+
+GyroExtROS::~GyroExtROS()
+{
+	gyro_pub_.shutdown();
+}
+
+void GyroExtROS::setTimeStamp(ros::Time stamp)
+{
+	stamp_ = stamp;
+}
+
+void GyroExtROS::gyroscopeExtEvent(float angle, float rate)
+{
+	// Build the Gyro message
+	gyro_msg_.header.stamp = stamp_;
+	gyro_msg_.header.frame_id = "base_link";
+
+    gyro_msg_.angle = angle;
+    gyro_msg_.rate = rate;
+
+    gyro_pub_.publish(gyro_msg_);
+}
--- robotino_node/src/PowerManagementROS.cpp
+++ robotino_node/src/PowerManagementROS.cpp
@@ -22,12 +22,20 @@ void PowerManagementROS::setTimeStamp(ros::Time stamp)
 	stamp_ = stamp;
 }
 
-void PowerManagementROS::readingsEvent(float current, float voltage)
+//void PowerManagementROS::readingsEvent(float current, float voltage)
+void PowerManagementROS::readingsEvent( float battery_voltage, float system_current, bool ext_power, int num_chargers, const char* batteryType, bool batteryLow, int batteryLowShutdownCounter )
 {
 	// Build the PowerReadings msg
 	power_msg_.stamp = ros::Time::now();
-	power_msg_.current = current;
-	power_msg_.voltage = voltage;
+	//power_msg_.current = current;
+	//power_msg_.voltage = voltage;
+
+	power_msg_.battery_voltage = battery_voltage;
+        power_msg_.system_current = system_current;
+	power_msg_.ext_power = ext_power;
+        power_msg_.num_chargers = num_chargers;
+        power_msg_.batteryLow = batteryLow;
+        power_msg_.batteryLowShutdownCounter = batteryLowShutdownCounter;
 
 	// Publish the msg
 	power_pub_.publish( power_msg_ );
